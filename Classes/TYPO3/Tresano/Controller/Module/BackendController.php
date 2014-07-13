4370f596d4940cea7badc97b532a8631c0a32da4
Simon Schaufelberger <simonschaufi@googlemail.com>
2014-07-13 14:17:25 +0200

    first commit

---
+++ Classes/TYPO3/Tresano/Controller/Module/BackendController.php
@@ -0,0 +1,18 @@
+<?php
+namespace TYPO3\Tresano\Controller\Module;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "TYPO3.Tresano".         *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+class BackendController extends \TYPO3\Neos\Controller\Module\AbstractModuleController {
+
+	/**
+	 * @return void
+	 */
+	public function indexAction() {
+	}
+}
